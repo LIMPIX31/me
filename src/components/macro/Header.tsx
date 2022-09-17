aed73278b1a4654ef637519010527d9869b95576
LIMPIX31 <limpix31@gmail.com>
2022-09-17 19:50:52 +0200

    feat: header

--- src/components/macro/Header.tsx
+++ src/components/macro/Header.tsx
@@ -2,6 +2,9 @@ import { FC } from 'react'
 import styled from 'styled-components'
 import { theme } from 'style/theme'
 import { Content } from 'components/micro/Content'
+import { BezierArrow } from 'svg/BezierArrow'
+import { NavLink } from 'react-router-dom'
+import { mix, rgb, rgba } from 'polished'
 
 const StyledHeader = styled.header`
   position: fixed;
@@ -28,13 +31,73 @@ const StyledHeader = styled.header`
 const HeaderContent = styled(Content)`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  gap: 5rem;
+  height: 100%;
+`
+
+const Logo = styled.img`
+  height: 80%;
+`
+
+const Arrow = styled(BezierArrow)`
+  height: 80%;
+  g {
+    stroke: ${theme.primary()};
+  }
+`
+
+const Nav = styled.nav`
+  display: flex;
+  align-self: stretch;
+`
+
+const NavItem = styled(NavLink)`
+  height: 100%;
+  padding: 0 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  line-height: 1.5rem;
+  font-family: Cartographer, sans-serif;
+  font-weight: 300;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-color: ${theme.front};
+  text-decoration: none;
+  transition: all 0.3s;
+  position: relative;
+  &:after {
+    content: '';
+    position: absolute;
+    left: 50%;
+    translate: -50%;
+    bottom: 1.1rem;
+    display: block;
+    width: 100%;
+    height: 1px;
+    mask: linear-gradient(90deg, transparent, white, transparent);
+  }
+
+  &.active {
+    background: ${theme.primary('45deg')};
+    &:after {
+      background: ${theme.secondary('45deg')};
+    }
+  }
 `
 
 export const Header: FC = () => {
   return (
     <StyledHeader>
-      <HeaderContent></HeaderContent>
+      <HeaderContent>
+        <Logo src={'/favicon.png'} />
+        <Arrow />
+        <Nav>
+          <NavItem to={'/cv'}>CV</NavItem>
+          <NavItem to={'/services'}>Услуги</NavItem>
+        </Nav>
+      </HeaderContent>
     </StyledHeader>
   )
 }
