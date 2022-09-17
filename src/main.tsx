bc83a3492e387e17e636d2cdb04d847d6c2a91f5
LIMPIX31 <limpix31@gmail.com>
2022-09-17 19:09:38 +0200

    feat: routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import { createRoot, hydrateRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import { Styles } from 'style/global'
 import { Fonts } from 'style/font'
 import { StrictMode } from 'react'
-import Home from 'routes/Home'
+import { BrowserRouter } from 'react-router-dom'
+import { App } from 'App'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Styles />
     <Fonts />
-    <Home />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
